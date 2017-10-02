06bdd24a1c2e0f87a725cd7b1cf2a946cfd5be9f
Maxime Lacroix <maxime.lacroix@epitech.eu>
2017-10-02 08:01:24 +0100

    fist push

---
+++ inc/main.hpp
@@ -0,0 +1,23 @@
+//
+// main.hpp for  in /home/kopiz/Workplace/301dannon/inc
+// 
+// Made by Tom Jeammet
+// Login   <tom.jeammet@epitech.eu>
+// 
+// Started on  Tue Sep 19 16:35:18 2017 Tom Jeammet
+// Last update Mon Oct  2 08:00:23 2017 ze
+//
+
+#ifndef __MAIN_HPP__
+# define __MAIN_HPP__
+
+# include <cstdlib>
+# include <iostream>
+# include <fstream>
+# include <vector>
+# include <string>
+# include <sstream>
+
+# include "exception.hpp"
+
+#endif /* ! __MAIN_HPP__ */
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// main.cpp for  in /home/ze/302separation
+// 
+// Made by ze
+// Login   <maxime.lacroix@epitech.eu>
+// 
+// Started on  Mon Oct  2 07:57:51 2017 ze
+// Last update Mon Oct  2 08:00:06 2017 ze
+//
+
+#include "main.hpp"
+
+int main(int ac, char **av)
+{
+  (void)ac;
+  (void)av;
+  return (84);
+}
