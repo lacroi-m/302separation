719b79dfb2e8ad4d77a8c25048bf50bea4adbb45
Maxime Lacroix <maxime.lacroix@epitech.eu>
2017-10-02 12:08:48 +0100

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,28 @@
 // Login   <maxime.lacroix@epitech.eu>
 // 
 // Started on  Mon Oct  2 07:57:51 2017 ze
-// Last update Mon Oct  2 08:00:06 2017 ze
+// Last update Mon Oct  2 12:07:23 2017 ze
 //
 
 #include "main.hpp"
+
+void	check(int ac, char **av)
+{
+  (void)ac;
+  (void)av;
+  throw (Err("Dont send me caca"));
+}
 
 int main(int ac, char **av)
 {
   (void)ac;
   (void)av;
-  return (84);
+  try{
+    check(ac, av);
+  }
+  catch (Err &err){
+    err.print_msg();
+    return (84);
+  }
+  return (0);
 }
